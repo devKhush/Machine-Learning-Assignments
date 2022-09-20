f273f8d293fdc8d457e8d7979eddb9c1900598cf
devKhush999 <khushdev7838@gmail.com>
2022-09-20 14:51:00 +0530

    Done: Ridge Regression TODO: LASSO Regression

--- Assignment-1/Test.py
+++ Assignment-1/Test.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Lin:
     def ok(self):
         print('ok parent')
@@ -21,3 +22,24 @@ class Grad_(Grad):
 
 obj = Grad_()
 obj.lmao()
+
+matrix = np.array( [[1,2,3,7],
+                    [8,4,6,3],
+                    [6,2,4,9]])
+model_parameter = np.array([ 3,
+                            -1,
+                            -2,
+                            4])
+
+new_parameter = np.zeros(len(matrix))
+            
+for j in range(len(model_parameter)):
+    jth_parameter = model_parameter[j]
+    model_parameter[j] = 0
+    
+    print(np.dot(matrix, model_parameter))
+
+    model_parameter[j] = jth_parameter
+    
+
+new_parameter
