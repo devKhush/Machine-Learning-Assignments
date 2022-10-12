2e6d835af176c8b486a78d638b4c98addbbc04ea
devKhush999 <khushdev7838@gmail.com>
2022-10-13 01:14:21 +0530

    Q2 and Q3 Done

--- Assignment-2/Section-B/utils.py
+++ Assignment-2/Section-B/utils.py
@@ -12,9 +12,13 @@ class CircleDataset:
         self.data = np.empty(shape=(10000, 6), dtype=np.float64)
         self.buildDataset()
 
+        # Try greater value of 'std' in noise, and observe that Perceptron can't be applied in Noisy
+        # Dataset, if they are not linearly separable
         if add_noise:
-            self.data[:, 0] = self.data[:, 0] + np.random.randn(self.n)
-            self.data[:, 1] = self.data[:, 1] + np.random.randn(self.n)
+            self.data[:, 0] = self.data[:, 0] + \
+                np.random.normal(loc=0, scale=0.1, size=self.n)
+            self.data[:, 1] = self.data[:, 1] + \
+                np.random.normal(loc=0, scale=0.1, size=self.n)
 
         return pd.DataFrame(data=self.data, columns=['x', 'y', 'center_x', 'center_y', 'radius', 'label'])
 
@@ -37,7 +41,11 @@ class CircleDataset:
 
 
 def split_circle_data_into_train_test(df: pd.DataFrame, partition_size, with_bias=True):
+    # Try PTA in greater dimesnion (with more features, by including centers for circle and radius)
+    # and we see it will give a decision boundary in greater dimension,
+    # which is not possible in 2-D. Also the accuracy is 100% with noisy data, in greater dimension.
     # x = df[df.columns[:-1]]
+
     x = df[['x', 'y']].copy(deep=True)
     y = df['label']
 
@@ -52,14 +60,20 @@ def split_circle_data_into_train_test(df: pd.DataFrame, partition_size, with_bia
     return x_train, y_train, x_test, y_test
 
 
-def split_bit_dataset(df: pd.DataFrame):
+def split_bit_dataset(df: pd.DataFrame, with_bias=True):
     x = df[df.columns[:-1]]
     y = df[df.columns[-1]]
-    x['bias_column'] = np.ones(x.shape[0])
+    if with_bias:
+        x['bias_column'] = np.ones(x.shape[0])
+    else:
+        x['bias_column'] = np.zeros(x.shape[0])
     return x, y
 
 
 class Perceptron:
+    def __init__(self) -> None:
+        self.epochs = 50
+
     def signum(self, value):
         return 1 if value >= 0 else -1
 
@@ -68,10 +82,8 @@ class Perceptron:
         self.weights = np.ones(x_train.shape[1])
         self.weights[-1] = 0.0
 
-        epoch = 0
-        while epoch < 10:
-            epoch += 1
-        # while True:
+        for epoch in range(self.epochs):
+            # while True:
             prev_weights = self.weights.copy()
 
             for i in range(x_train.shape[0]):
